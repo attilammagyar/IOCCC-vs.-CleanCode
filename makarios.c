41f13d02924c996931e3df75ffdda588d7cf5a1b
Attila M. Magyar <attila.m.magyar@gmail.com>
2011-11-23 18:56:12 +0100

    Making the structure visible while eliminating some warnings
        * Including stdio.h and returning 0 from main() eliminates
          some of the compiler warnings.
        * Adding some indentation and braces to highlight the
          basic structure of the code.
        * Replacing arguments with local variables:
    
            * It's easy to see that both a and i get overwritten
              before usage, so there's no point receiving them as
              arguments.
            * The first argument of main() is n which is always
              the number of command line arguments. It's used as
              the starting point of the while loop. Easy to test
              by experiment, this program does not print numbers
              smaller than the number of command line arguments
              (note that the output is in octal base), so if one
              would want to see only numbers greater than eg. 64
              (100 in octal base), the program should be invoked
              like this:
    
                    ./makarios `seq 1 64`
    
              This is not very useful, so I decided to drop this
              feature. Variable n will start from 1, as when the
              program is invoked without arguments, the first
              parameter of main() is 1.
            * The trickiest variable of main() is m. Note that the
              expression in the conditional of the for loop is
              built around a ternary operator. The conditional of
              the ternary is a<i, and m is only written in the
              positive branch. In each iteration of the while loop,
              variable a starts from 0, which is smaller than i in
              the first iteration of the for loop (note that before
              the for loop, i is always positive), so the positive
              part of the ternary is evaluated in the first
              iteration, which means m gets overwritten. Thus, m
              can be eliminated from the argument list. :-)

--- makarios.c
+++ makarios.c
@@ -1,4 +1,18 @@
+#include <stdio.h>
+
 #define STOP_AT 522233
 
-main(n,i,a,m){while((i=++n) <= STOP_AT)
-for(a=0;a<i?a=a*8+i%8,i/=8,m=a==i|a/8==i,1:(n-++m||printf("%o\n",n))&&n%m;);}
+int main(int argc, char* argv[])
+{
+	int n = 1,
+		i, a, m;
+
+	while((i=++n) <= STOP_AT)
+	{
+		for(a=0;a<i?a=a*8+i%8,i/=8,m=a==i|a/8==i,1:(n-++m||printf("%o\n",n))&&n%m;)
+		{
+		}
+	}
+
+	return 0;
+}
