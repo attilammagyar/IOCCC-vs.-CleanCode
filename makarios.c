86182ebee0aec6102d026d2109c8887ca60325ce
Attila M. Magyar <attila.m.magyar@gmail.com>
2011-11-23 19:07:35 +0100

    Converting the for loop to a while
        * Variables a and m are not used outside the body of the
          while loop, they can be moved closer to their use.
        * Converting the for loop to a while might help breaking
          down the magic expression.

--- makarios.c
+++ makarios.c
@@ -5,11 +5,14 @@
 int main(int argc, char* argv[])
 {
 	int n = 1,
-		i, a, m;
+		i;
 
 	while((i=++n) <= STOP_AT)
 	{
-		for(a=0;a<i?a=a*8+i%8,i/=8,m=a==i|a/8==i,1:(n-++m||printf("%o\n",n))&&n%m;)
+		int a = 0,
+			m;
+
+		while (a<i?a=a*8+i%8,i/=8,m=a==i|a/8==i,1:(n-++m||printf("%o\n",n))&&n%m)
 		{
 		}
 	}
