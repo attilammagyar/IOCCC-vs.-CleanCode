ea2149d1487f6c391f07631a46a46565001156f2
Attila M. Magyar <attila.m.magyar@gmail.com>
2011-11-24 00:26:52 +0100

    Oops, made a mistake: moving definition of digit down into the loop

--- makarios.c
+++ makarios.c
@@ -27,12 +27,11 @@ static int
 magic_algorithm(int number)
 {
 	int reversed_digits = 0,
-		remaining_digits = number,
-		digit;
+		remaining_digits = number;
 
 	while (reversed_digits < remaining_digits)
 	{
-		digit = get_last_octal_digit(remaining_digits);
+		int digit = get_last_octal_digit(remaining_digits);
 		reversed_digits = append_octal_digit(reversed_digits, digit);
 		remaining_digits = remove_last_octal_digit(remaining_digits);
 	}
