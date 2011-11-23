3a861301c8ef4088f0dd0536564380adcb47e3df
Attila M. Magyar <attila.m.magyar@gmail.com>
2011-11-23 23:07:40 +0100

    Extracting functions from main()
        * The format string for printf() is an ugly implementation
          detail, it's better hidden inside a small private
          function buried deep down in the code. Finding a
          descriptive name for that function is easy knowing that
          %o outputs a number in octal base.
        * Variables a and i are only used by the first inner while
          loop and in the boolean expression in the if statement.
          There's a function hidden there, the return value is the
          result of the boolean epression, the algorithm producing
          that result is the loop. It's still unknown what this
          function does, so let's give it a temporary name,
          for example magic_function() will do just fine.
        * The second inner loop uses both n and m, but n is not
          changed by it. The second if statement also uses both
          variables in the condition. This calls for another
          extracted function, because this code is some other magic
          algorithm, in other words, an ugly implementation detail
          that should be hidden. Coming up with a good name would
          require understanding the extracted function, we'll get
          back to that later. Now other_magic_function() is more
          than enough.

--- makarios.c
+++ makarios.c
@@ -2,34 +2,57 @@
 
 #define STOP_AT 522233
 
+static int magic_algorithm(int number);
+static int other_magic_algorithm(int number);
+static void print_octal(int number);
+
 int main(int argc, char* argv[])
 {
 	int n;
 
 	for (n = 2; n <= STOP_AT; ++n)
 	{
-		int a = 0,
-			i = n,
-			m;
-
-		while (a < i)
-		{
-			a = a*8 + i%8;
-			i /= 8;
-		}
-
-		if (!((a == i) || (a/8 == i)))
+		if (!magic_algorithm(n))
 			continue;
 
-		m = 2;
-		while (0 != n%m)
-		{
-			++m;
-		}
-
-		if (m == n)
-			printf("%o\n", n);
+		if (other_magic_algorithm(n))
+			print_octal(n);
 	}
 
 	return 0;
+}
+
+static int
+magic_algorithm(int number)
+{
+	int a = 0,
+		i = number;
+
+	while (a < i)
+	{
+		a = a*8 + i%8;
+		i /= 8;
+	}
+
+	return (a == i) || (a/8 == i);
+}
+
+static int
+other_magic_algorithm(int number)
+{
+	int m;
+
+	m = 2;
+	while (0 != number%m)
+	{
+		++m;
+	}
+
+	return m == number;
+}
+
+static void
+print_octal(int number)
+{
+	printf("%o\n", number);
 }
