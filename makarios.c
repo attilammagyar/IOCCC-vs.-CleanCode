d3e5388628e1674a0c4e1f3a847e577cfae877ba
Attila M. Magyar <attila.m.magyar@gmail.com>
2011-11-23 19:12:01 +0100

    Converting pre-test loop to post-test
        This makes room to break down the magic ternary.

--- makarios.c
+++ makarios.c
@@ -10,11 +10,12 @@ int main(int argc, char* argv[])
 	while((i=++n) <= STOP_AT)
 	{
 		int a = 0,
-			m;
+			m, is_not_ready;
 
-		while (a<i?a=a*8+i%8,i/=8,m=a==i|a/8==i,1:(n-++m||printf("%o\n",n))&&n%m)
+		do
 		{
-		}
+			is_not_ready = a<i?a=a*8+i%8,i/=8,m=a==i|a/8==i,1:(n-++m||printf("%o\n",n))&&n%m;
+		} while (is_not_ready);
 	}
 
 	return 0;
