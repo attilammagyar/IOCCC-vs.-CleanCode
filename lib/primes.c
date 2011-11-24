dca0b02f7ff72604e275b14a47caf5071707f9ec
Attila M. Magyar <attila.m.magyar@gmail.com>
2011-11-25 00:28:56 +0100

    Primes.c: adding a note about possible optimization

--- lib/primes.c
+++ lib/primes.c
@@ -16,6 +16,9 @@ find_smallest_divisor_greater_than_one(int number)
 {
 	/* FIXME: handle negative numbers */
 
+	/* OPTIMIZATION FIXME: The smallest divisor is always less than
+						   the square root of the number. */
+
 	int smallest_divisor = 2;
 
 	while (!is_divisable(number, smallest_divisor))
