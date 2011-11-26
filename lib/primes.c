d14461f8e9673ae1338e1692f5b757d5acdb5044
Attila M. Magyar <attila.m.magyar@gmail.com>
2011-11-26 23:14:17 +0100

    Primes.c: cleaning up the code after optimization
        * Assigning values right before using them.
        * Replacing the while loop with a for loop for
          better readability.

--- lib/primes.c
+++ lib/primes.c
@@ -3,7 +3,7 @@
 #include <math.h>
 #include <stdlib.h>
 
-static int find_divisor_closest_to_one(int number);
+static int is_divisable_by_non_trivial_divisor(int number);
 
 int
 is_prime(int number)
@@ -11,33 +11,36 @@ is_prime(int number)
 	if (number == 0 || number == 1 || number == -1)
 		return 0;
 
-	return find_divisor_closest_to_one(number) == number;
+	return !is_divisable_by_non_trivial_divisor(number);
 }
 
+static int is_even(int number);
 static int is_divisable(int dividend, int divisor);
 
 static int
-find_divisor_closest_to_one(int number)
+is_divisable_by_non_trivial_divisor(int number)
 {
-	int divisor_candidate = 3,
-		signum = (number < 0) ? -1 : 1,
-		absolute_value = abs(number),
-		square_root = (int)sqrt((double)absolute_value) + 1;
+	int absolute_value = abs(number),
+		square_root, divisor;
 
-	if (is_divisable(absolute_value, 2))
-		return signum * 2;
+	if (is_even(absolute_value))
+		return 2 != absolute_value;
 
-	while (!is_divisable(absolute_value, divisor_candidate))
+	square_root = (int)sqrt((double)absolute_value) + 1;
+
+	for (divisor = 3; divisor <= square_root; divisor += 2)
 	{
-		divisor_candidate += 2;
-		if (divisor_candidate > square_root)
-		{
-			divisor_candidate = absolute_value;
-			break;
-		}
+		if (is_divisable(number, divisor))
+			return 1;
 	}
 
-	return signum * divisor_candidate;
+	return 0;
+}
+
+static int
+is_even(int number)
+{
+	return !(number & 1);
 }
 
 static int
